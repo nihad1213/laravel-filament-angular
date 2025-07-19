40cfabb0492f026c5377b87d15ac6e4c860c7cfc
nihad1213 <nihad.nemetli@gmail.com>
2025-07-19 22:56:02 +0400

    feat: GameResource created

---
+++ laravel-filament/app/Filament/Resources/GameResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\GameResource\Pages;
+use App\Filament\Resources\GameResource\RelationManagers;
+use App\Models\Game;
+use Filament\Forms;
+use Filament\Forms\Form;
+use App\Models\Category;
+use App\Models\Platform;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class GameResource extends Resource
+{
+    protected static ?string $model = Game::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\Textarea::make('description')
+                    ->rows(10)
+                    ->cols(20)
+                    ->required(),
+                Forms\Components\FileUpload::make('cover_image')
+                    ->required()
+                    ->image(),
+                Forms\Components\DatePicker::make('release_date')
+                    ->required()
+                    ->maxDate(now()),
+                Forms\Components\Select::make('categories')
+                    ->label('Categories')
+                    ->multiple()
+                    ->relationship('categories', 'name')
+                    ->preload(),
+
+                Forms\Components\Select::make('platforms')
+                    ->label('Platforms')
+                    ->multiple()
+                    ->relationship('platforms', 'name')
+                    ->preload(),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListGames::route('/'),
+            'create' => Pages\CreateGame::route('/create'),
+            'edit' => Pages\EditGame::route('/{record}/edit'),
+        ];
+    }
+}
