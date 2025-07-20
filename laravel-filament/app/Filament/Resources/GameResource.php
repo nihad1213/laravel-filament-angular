c83ee99b84c49fb185846530e1619b0e059200d1
nihad1213 <nihad.nemetli@gmail.com>
2025-07-20 11:55:58 +0400

    icon changed

--- laravel-filament/app/Filament/Resources/GameResource.php
+++ laravel-filament/app/Filament/Resources/GameResource.php
@@ -19,7 +19,7 @@ class GameResource extends Resource
 {
     protected static ?string $model = Game::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
 
     public static function form(Form $form): Form
     {
